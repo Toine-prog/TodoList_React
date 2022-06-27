adf1a83cc5afe4513428e6947f8abff406165dd2
Antoine <a.ruchot@outlook.com>
2022-06-27 23:38:58 +0200

    système d'ajout item ok

--- todo_list_app/src/screens/UseState.jsx
+++ todo_list_app/src/screens/UseState.jsx
@@ -1,29 +1,56 @@
 import React from 'react';
+import { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
 import { FaPlusSquare, FaEdit } from 'react-icons/fa';
 import { ImBin } from 'react-icons/im';
 
+
 function UseState() {
+
+  const OnChangeItemText = (e) => {
+    setItemToAdd(e.target.value);
+  }
+  
+  const addItem = () => { 
+    if(itemToAdd !== "") {
+      setItems(prevItems => [...prevItems, {
+        id: prevItems.length,
+        value: itemToAdd
+      }]);
+  
+      setItemToAdd("")
+    }
+  }
+
+  const deleteItem = (itemIdToDelete) => {
+
+    setItems(items.filter(item => {
+      return item.id !== itemIdToDelete;
+    }));
+  }
+
+  const [items, setItems] = useState([]);
+  const [itemToAdd, setItemToAdd] = useState("");
+
   return (
     <div className="box">
       <h1>Use State Todo List</h1>
       <Form>
         <div className="flexHorizontal">
-          <Form.Control placeholder="Add an item" />
-          <Button variant="success"><FaPlusSquare /></Button>
+          <Form.Control placeholder="Add an item" value={itemToAdd} onChange={OnChangeItemText}/>
+          <Button variant="success" onClick={addItem}><FaPlusSquare /></Button>
         </div>
       </Form>
       <div className="listItems">
-        <div className="flexHorizontal">
-          <Form.Control value="Add an item" />
-          <Button><FaEdit /></Button>
-          <Button variant="danger"><ImBin /></Button>
-        </div>
-        <div className="flexHorizontal">
-          <Form.Control value="Add an item" />
-          <Button><FaEdit /></Button>
-          <Button variant="danger"><ImBin /></Button>
-        </div>
+        {items && items.map((item) => {
+            return (
+              <div className="flexHorizontal" key={item.id}>
+                <Form.Control value={item.value} />
+                <Button><FaEdit /></Button>
+                {/* <Button variant="danger" onClick={deleteItem(item.id)}><ImBin /></Button> */}
+            </div>
+            )
+        })}
       </div>
     </div>
   )
