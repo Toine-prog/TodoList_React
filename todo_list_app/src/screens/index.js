6d1f9568f5fda93d6d809ebf46b31c1259cbed99
Antoine <a.ruchot@outlook.com>
2022-06-27 12:22:48 +0200

    importation des différentes pages

---
+++ todo_list_app/src/screens/index.js
@@ -0,0 +1,6 @@
+import UseContext from "./UseContext";
+import UseState from "./UseState";
+import UseEffects from "./UseEffects";
+import UseRef from "./UseRef";
+
+export { UseContext, UseEffects, UseState, UseRef }
