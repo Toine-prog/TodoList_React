c5de0aa9b316b0e358c8729cd5ead2f303ba47c0
Antoine <a.ruchot@outlook.com>
2022-06-28 12:20:59 +0200

    edition + suppression ok

--- todo_list_app/src/screens/UseState.jsx
+++ todo_list_app/src/screens/UseState.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
-import { FaPlusSquare, FaEdit } from 'react-icons/fa';
+import { FaPlusSquare, FaEdit, FaCheckSquare } from 'react-icons/fa';
 import { ImBin } from 'react-icons/im';
 
 
@@ -10,12 +10,17 @@ function UseState() {
   const OnChangeItemText = (e) => {
     setItemToAdd(e.target.value);
   }
+
+  const OnChangeItemEdit = (e) => {
+
+  }
   
   const addItem = () => { 
     if(itemToAdd !== "") {
       setItems(prevItems => [...prevItems, {
         id: prevItems.length,
-        value: itemToAdd
+        value: itemToAdd,
+        readOnly: true
       }]);
   
       setItemToAdd("")
@@ -29,6 +34,17 @@ function UseState() {
     }));
   }
 
+  const editItem = (itemIdToEdit, e) => {
+    const newItems = items.map(item => {
+      if(item.id === itemIdToEdit) {
+        item.readOnly = !item.readOnly;
+        item.value = e.target.value;
+      }
+      return item;
+    });
+    setItems(newItems);
+  }
+
   const [items, setItems] = useState([]);
   const [itemToAdd, setItemToAdd] = useState("");
 
@@ -37,19 +53,29 @@ function UseState() {
       <h1>Use State Todo List</h1>
       <Form>
         <div className="flexHorizontal">
-          <Form.Control placeholder="Add an item" value={itemToAdd} onChange={OnChangeItemText}/>
+          <Form.Control placeholder="Add an item" value={itemToAdd} onChange={OnChangeItemText} />
           <Button variant="success" onClick={addItem}><FaPlusSquare /></Button>
         </div>
       </Form>
       <div className="listItems">
         {items && items.map((item) => {
+          if(item.readOnly) {
             return (
               <div className="flexHorizontal" key={item.id}>
-                <Form.Control value={item.value} />
-                <Button><FaEdit /></Button>
-                {/* <Button variant="danger" onClick={deleteItem(item.id)}><ImBin /></Button> */}
+                <Form.Control value={item.value} readOnly={item.readOnly}/>
+                <Button onClick={(e) => editItem(item.id, e)}><FaEdit /></Button>
+                <Button variant="danger" onClick={() => deleteItem(item.id)}><ImBin /></Button>
             </div>
             )
+          }
+          else {
+            return (
+              <div className="flexHorizontal" key={item.id}>
+                <Form.Control value={item.value} readOnly={item.readOnly}/>
+                <Button variant="success" onClick={(e) => editItem(item.id, e)}><FaCheckSquare /></Button>
+            </div>
+            )
+          }
         })}
       </div>
     </div>
