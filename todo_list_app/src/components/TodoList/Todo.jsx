5033ad487424f048c7b6021eb29b546cec16aefd
Antoine <a.ruchot@outlook.com>
2022-06-30 20:01:41 +0200

    amélioration du placement des fonctions

--- todo_list_app/src/components/TodoList/Todo.jsx
+++ todo_list_app/src/components/TodoList/Todo.jsx
@@ -1,26 +1,69 @@
 import React from 'react';
+import { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
 import { FaEdit, FaCheckSquare } from 'react-icons/fa';
 import { ImBin } from 'react-icons/im';
 
-const Todo = ({item, editItem, deleteItem, validItem, editedItem, OnChangeEditingItem}) => {
-    if(item.readOnly) {
-        return (
-            <div className="flexHorizontal" key={item.id}>
+const Todo = ({item, items, setItems}) => {
+    const [editedItem, setEditedItem] = useState("");
+    const [isEditing, setIsEditing] = useState(false);
+    
+    const OnChangeEditingItem = (e) => {
+        setEditedItem(e.target.value);
+    }
+
+    const deleteItem = (itemIdToDelete) => {
+        setItems(items.filter(item => {
+        return item.id !== itemIdToDelete;
+        }));
+    }
+
+    const editItem = (itemIdToEdit) => {
+        const newItems = items.map(item => {
+        if(!isEditing) {
+            if(item.id === itemIdToEdit) {
+            item.readOnly = !item.readOnly;
+            setEditedItem(item.value);
+            setIsEditing(true);
+            }
+        }
+        else {
+            alert("An item is already being editing !");
+        }
+        return item;
+        });
+        setItems(newItems);
+    }
+
+    const validItem = (itemIdToValid) => {
+        const newItems = items.map(item => {
+        if(item.id === itemIdToValid) {
+            item.readOnly = !item.readOnly;
+            item.value = editedItem;
+        }
+        return item;
+        });
+        setItems(newItems);
+        setIsEditing(false);
+    }
+
+    return(
+        <>
+         {item.readOnly ?
+        <div className="flexHorizontal" key={item.id}>
             <Form.Control value={item.value} readOnly={item.readOnly}/>
             <Button onClick={(e) => editItem(item.id)}><FaEdit /></Button>
             <Button variant="danger" onClick={() => deleteItem(item.id)}><ImBin /></Button>
         </div>
-        )
-        }
-        else {
-        return (
-            <div className="flexHorizontal" key={item.id}>
+        :
+        <div className="flexHorizontal" key={item.id}>
             <Form.Control value={item.readOnly ? item.value : editedItem} readOnly={item.readOnly} onChange={OnChangeEditingItem}/>
             <Button variant="success" onClick={() => validItem(item.id)}><FaCheckSquare /></Button>
         </div>
-        )
         }
+        </>
+    )
+    
 }
 
 export default Todo
--- todo_list_app/src/components/TodoList/Todos.jsx
+++ todo_list_app/src/components/TodoList/Todos.jsx
@@ -1,63 +1,13 @@
 import React from 'react';
-import { useState } from 'react';
 import { Todo } from './';
 
 const Todos = ({items, setItems}) => {
-
-    const [editedItem, setEditedItem] = useState("");
-    const [isEditing, setIsEditing] = useState(false);
-    
-    const OnChangeEditingItem = (e) => {
-        setEditedItem(e.target.value);
-    }
-
-    const deleteItem = (itemIdToDelete) => {
-        setItems(items.filter(item => {
-        return item.id !== itemIdToDelete;
-        }));
-    }
-
-    const editItem = (itemIdToEdit) => {
-        const newItems = items.map(item => {
-        if(!isEditing) {
-            if(item.id === itemIdToEdit) {
-            item.readOnly = !item.readOnly;
-            setEditedItem(item.value);
-            setIsEditing(true);
-            }
-        }
-        else {
-            alert("An item is already being editing !");
-        }
-        return item;
-        });
-        setItems(newItems);
-    }
-
-    const validItem = (itemIdToValid) => {
-        const newItems = items.map(item => {
-        if(item.id === itemIdToValid) {
-            item.readOnly = !item.readOnly;
-            item.value = editedItem;
-        }
-        return item;
-        });
-        setItems(newItems);
-        setIsEditing(false);
-    }
 
   return (
     <div className="listItems">
         {items && items.map((item) => {
           return(
-          <Todo 
-            item={item} 
-            editItem={editItem} 
-            deleteItem={deleteItem} 
-            validItem={validItem} 
-            editedItem={editedItem} 
-            OnChangeEditingItem={OnChangeEditingItem}
-          />
+          <Todo item={item} items={items} setItems={setItems}/>
           )
         })}
       </div>
