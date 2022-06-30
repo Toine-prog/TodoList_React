95a2ed55e7c24ee38c4667a0d32ed06bc20f6273
Antoine <a.ruchot@outlook.com>
2022-06-30 12:45:07 +0200

    fonction d'ajout mis dans TodoForm

--- todo_list_app/src/components/TodoList/TodoForm.jsx
+++ todo_list_app/src/components/TodoList/TodoForm.jsx
@@ -3,12 +3,31 @@ import { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
 import { FaPlusSquare} from 'react-icons/fa';
 
-const TodoForm = ({itemToAdd, handleChange, addItem}) => {
+const TodoForm = ({setItems}) => {
+
+    const [itemToAdd, setItemToAdd] = useState("");
+
+    const OnChangeItemText = (e) => {
+        setItemToAdd(e.target.value);
+      }
+      
+      const addItem = (e) => { 
+        e.preventDefault();
+        if(itemToAdd !== "") {
+          setItems(prevItems => [...prevItems, {
+            id: prevItems.length,
+            value: itemToAdd,
+            readOnly: true,
+          }]);
+          setItemToAdd("");
+        }
+      }
+    
   return (
     <div>
         <Form>
         <div className="flexHorizontal">
-          <Form.Control placeholder="Add an item" value={itemToAdd} onChange={handleChange} />
+          <Form.Control placeholder="Add an item" value={itemToAdd} onChange={OnChangeItemText} />
           <Button variant="success" onClick={addItem}><FaPlusSquare /></Button>
         </div>
       </Form>
--- todo_list_app/src/screens/UseState.jsx
+++ todo_list_app/src/screens/UseState.jsx
@@ -5,28 +5,11 @@ import { TodoForm, Todos } from '../components/TodoList'
 
 function UseState() {
 
-  const OnChangeItemText = (e) => {
-    setItemToAdd(e.target.value);
-  }
-  
-  const addItem = (e) => { 
-    e.preventDefault();
-    if(itemToAdd !== "") {
-      setItems(prevItems => [...prevItems, {
-        id: prevItems.length,
-        value: itemToAdd,
-        readOnly: true,
-      }]);
-      setItemToAdd("");
-    }
-  }
-
   const [items, setItems] = useState(() => {
     const saved = localStorage.getItem("items");
     const initialValue = JSON.parse(saved);
     return initialValue || [];
   });
-  const [itemToAdd, setItemToAdd] = useState("");
 
   useEffect(() => {
     localStorage.setItem("items", JSON.stringify(items));
@@ -35,8 +18,8 @@ function UseState() {
   return (
     <div className="box">
       <h1>Use State Todo List</h1>
-      <TodoForm itemToAdd={itemToAdd}  handleChange = {OnChangeItemText}  addItem = {addItem} />
-      <Todos items={items} />
+      <TodoForm setItems={setItems}/>
+      <Todos items={items} setItems={setItems} />
     </div>
   )
 }
