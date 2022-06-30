c178e84465cff7b41de225b60bb34b28bf29e8f6
Antoine <a.ruchot@outlook.com>
2022-06-30 12:38:21 +0200

    fonctionnement todoList avec ajout de composants

--- todo_list_app/src/components/TodoList/Todo.jsx
+++ todo_list_app/src/components/TodoList/Todo.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from 'react';
+import { Form, Button } from 'react-bootstrap';
+import { FaEdit, FaCheckSquare } from 'react-icons/fa';
+import { ImBin } from 'react-icons/im';
 
-const Todo = () => {
-  return (
-    <div>Todo</div>
-  )
+const Todo = ({item, editItem, deleteItem, validItem, editedItem, OnChangeEditingItem}) => {
+    if(item.readOnly) {
+        return (
+            <div className="flexHorizontal" key={item.id}>
+            <Form.Control value={item.value} readOnly={item.readOnly}/>
+            <Button onClick={(e) => editItem(item.id)}><FaEdit /></Button>
+            <Button variant="danger" onClick={() => deleteItem(item.id)}><ImBin /></Button>
+        </div>
+        )
+        }
+        else {
+        return (
+            <div className="flexHorizontal" key={item.id}>
+            <Form.Control value={item.readOnly ? item.value : editedItem} readOnly={item.readOnly} onChange={OnChangeEditingItem}/>
+            <Button variant="success" onClick={() => validItem(item.id)}><FaCheckSquare /></Button>
+        </div>
+        )
+        }
 }
 
 export default Todo
--- todo_list_app/src/screens/UseState.jsx
+++ todo_list_app/src/screens/UseState.jsx
@@ -1,19 +1,12 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
-import { Form, Button } from 'react-bootstrap';
-import { FaEdit, FaCheckSquare } from 'react-icons/fa';
-import { TodoForm, Todo, Todos } from '../components/TodoList'
-import { ImBin } from 'react-icons/im';
+import { TodoForm, Todos } from '../components/TodoList'
 
 
 function UseState() {
 
   const OnChangeItemText = (e) => {
     setItemToAdd(e.target.value);
-  }
-
-  const OnChangeEditingItem = (e) => {
-    setEditedItem(e.target.value);
   }
   
   const addItem = (e) => { 
@@ -28,50 +21,12 @@ function UseState() {
     }
   }
 
-  const deleteItem = (itemIdToDelete) => {
-
-    setItems(items.filter(item => {
-      return item.id !== itemIdToDelete;
-    }));
-  }
-
-  const editItem = (itemIdToEdit) => {
-    const newItems = items.map(item => {
-      if(!isEditing) {
-        if(item.id === itemIdToEdit) {
-          item.readOnly = !item.readOnly;
-          setEditedItem(item.value);
-          setIsEditing(true);
-        }
-      }
-      else {
-        alert("An item is already being editing !");
-      }
-      return item;
-    });
-    setItems(newItems);
-  }
-
-  const validItem = (itemIdToValid) => {
-    const newItems = items.map(item => {
-      if(item.id === itemIdToValid) {
-        item.readOnly = !item.readOnly;
-        item.value = editedItem;
-      }
-      return item;
-    });
-    setItems(newItems);
-    setIsEditing(false);
-  }
-
   const [items, setItems] = useState(() => {
     const saved = localStorage.getItem("items");
     const initialValue = JSON.parse(saved);
     return initialValue || [];
   });
   const [itemToAdd, setItemToAdd] = useState("");
-  const [editedItem, setEditedItem] = useState("");
-  const [isEditing, setIsEditing] = useState(false);
 
   useEffect(() => {
     localStorage.setItem("items", JSON.stringify(items));
@@ -81,27 +36,7 @@ function UseState() {
     <div className="box">
       <h1>Use State Todo List</h1>
       <TodoForm itemToAdd={itemToAdd}  handleChange = {OnChangeItemText}  addItem = {addItem} />
-      <div className="listItems">
-        {items && items.map((item) => {
-          if(item.readOnly) {
-            return (
-              <div className="flexHorizontal" key={item.id}>
-                <Form.Control value={item.value} readOnly={item.readOnly}/>
-                <Button onClick={(e) => editItem(item.id)}><FaEdit /></Button>
-                <Button variant="danger" onClick={() => deleteItem(item.id)}><ImBin /></Button>
-            </div>
-            )
-          }
-          else {
-            return (
-              <div className="flexHorizontal" key={item.id}>
-                <Form.Control value={item.readOnly ? item.value : editedItem} readOnly={item.readOnly} onChange={OnChangeEditingItem}/>
-                <Button variant="success" onClick={() => validItem(item.id)}><FaCheckSquare /></Button>
-            </div>
-            )
-          }
-        })}
-      </div>
+      <Todos items={items} />
     </div>
   )
 }
